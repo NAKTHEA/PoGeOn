d04d6ec320d89559dbfac9bc3143465e43148921
Diego Marin <diegomr94@icloud.com>
2023-12-17 18:20:51 +0100

    A.P.I proyecto2

---
+++ A.P.I. proyecto2/Server.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('dev'))
+
+// Middleware de 404
+app.use((req, res) => {
+  res.status(404).send({
+    status: 'error',
+    message: 'Not found'
+  })
+})
+
+//Midleware de gestión de errores
+
+app.use((error, req, res, next) =>{
+    console.error(error);
+
+    res.status(error.httpStatus 500).send({
+        status:'error',
+        message:'error.message',
+    });
+});
+
+//Lanzamos el servidor 
+app.listen(3000, () => {
+    console.log('Servidor funcionando! 👻');
+  });
+  
